535e7878b57fbe1c0d343bbf27ba767a79a69a4b
Ross Phillips <rphillips-nz@users.noreply.github.com>
2020-10-26 00:03:29 +1300

    Rough demo working

---
+++ README.md
@@ -0,0 +1,7 @@
+# Off Orbit GB Site
+
+Simple demo site for [Off Orbit GB](https://github.com/rphillips-nz/off-orbit-gb/).
+
+Uses [https://github.com/juchi/gameboy.js](https://github.com/juchi/gameboy.js) to run the ROM.
+
+It's pretty much unplayable on mobile, but it does run.
---
+++ js/index.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var ajaxReader = new GameboyJS.RomAjaxReader();
+	var canvas = document.getElementById('gameboy-screen');
+
+	var gameboy = new GameboyJS.Gameboy(canvas, {
+		romReaders: [ajaxReader]
+	});
+
+	ajaxReader.loadFromUrl('/roms/off-orbit.gb');
+
+	// Prevent the default action for space and arrow keys to stop scrolling while playing
+	window.addEventListener('keydown', function(e) {
+		if ([32, 37, 38, 39, 40].indexOf(e.keyCode) >= 0) {
+			e.preventDefault();
+		}
+	}, false);
+
+	var keys = {
+		START: false,
+		SELECT: false,
+		B: false,
+		A: false,
+		DOWN: false,
+		UP: false,
+		LEFT: false,
+		RIGHT: false
+	};
+
+	function createButtonListener(key) {
+		var id = 'button-' + key.toLowerCase();
+		var buttonEl = document.getElementById(id);
+
+		buttonEl.addEventListener('mousedown', function () {
+			if (keys[key]) {
+				return;
+			}
+
+			keys[key] = true;
+			gameboy.input.pressKey(key);
+			buttonEl.classList.add('gameboy-button__pulse');
+
+			setTimeout(function () {
+				buttonEl.classList.remove('gameboy-button__pulse');
+				gameboy.input.releaseKey(key);
+				keys[key] = false;
+			}, 30);
+		});
+	}
+
+	var keysKeys = Object.keys(keys);
+	for (var i = 0; i < keysKeys.length; i++) {
+		createButtonListener(keysKeys[i]);
+	}
+});
